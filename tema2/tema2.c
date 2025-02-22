dc81dc43ac70cde2ab98cc60e0e3d803102fc139
Cauã <cauacustodiopereira@gmail.com>
2025-02-22 16:26:28 -0300

    Tema 2 Desafio Mestre

--- tema2/tema2.c
+++ tema2/tema2.c
@@ -3,7 +3,7 @@
 int main()
 {
     char state_1 = 'A';
-    char cod_1[3] = "A03";
+    char cod_1[4] = "A03";
     char name_1[20] = "SerraVerde";
     unsigned long int population_1 = 150500;
     float area_1 = 620.8;
@@ -14,7 +14,7 @@ int main()
     float super_1;
 
     char state_2 = 'B';
-    char cod_2[3] = "B02";
+    char cod_2[4] = "B02";
     char name_2[20] = "PortoVerde";
     unsigned long int population_2 = 20000;
     float area_2 = 780.8;
@@ -64,151 +64,160 @@ int main()
     // printf("Turistic: ");
     // scanf("%d", &turistic_2);
 
-    printf("Enter a number of the attribute you will compare with: \n"
-           "1 = Name || 2 = Population || 3 = Area || 4 = PIB || 5 = Turistic || 6 = Density || 7 = Per Capita \n");
+    printf("Enter a number of the attribute you will compare with first: \n"
+           "1 = Population || 2 = Area || 3 = PIB || 4 = Turistic || 5 = Density || 6 = Per Capita \n");
 
-    int option;
-    scanf("%d", &option);
+    int option1;
+    int result1;
+    scanf("%d", &option1);
 
-    switch (option)
+    switch (option1)
     {
     case 1:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
+        printf("Attribute used to compare: Population \n"
+               "Population of Card 1: %lu \n"
+               "Population of Card 2: %lu \n",
+               population_1, population_2);
+        result1 = population_1 > population_2 ? 1 : 0;
         break;
     case 2:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
-        printf("Attribute used to compare: Population \n"
-            "Population of Card 1: %d \n"
-            "Population of Card 2: %d \n",
-            population_1, population_2);
-        if (population_1 > population_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (population_1 == population_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
+        printf("Attribute used to compare: Area \n"
+               "Area of Card 1: %.2f \n"
+               "Area of Card 2: %.2f \n",
+               area_1, area_2);
+        result1 = area_1 > area_2 ? 1 : 0;
         break;
     case 3:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
-        printf(
-            "Attribute used to compare: Area \n"
-            "Area of Card 1: %.2f \n"
-            "Area of Card 2: %.2f \n",
-            area_1, area_2);
-        if (area_1 > area_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (area_1 == area_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
-        
+        printf("Attribute used to compare: PIB \n"
+               "PIB of Card 1: %.2f \n"
+               "PIB of Card 2: %.2f \n",
+               pib_1, pib_2);
+        result1 = pib_1 > pib_2 ? 1 : 0;
         break;
     case 4:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
-        printf(
-            "Attribute used to compare: PIB \n"
-            "PIB of Card 1: %.2f \n"
-            "PIB of Card 2: %.2f \n",
-            pib_1, pib_2);
-        if (pib_1 > pib_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (pib_1 == pib_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
-        
+        printf("Attribute used to compare: Turistic \n"
+               "Turistic of Card 1: %d \n"
+               "Turistic of Card 2: %d \n",
+               turistic_1, turistic_2);
+        result1 = turistic_1 > turistic_2 ? 1 : 0;
         break;
     case 5:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
-        printf(
-            "Attribute used to compare: Turistic \n"
-            "Turistic of Card 1: %d \n"
-            "Turistic of Card 2: %d \n",
-            turistic_1, turistic_2);
-        if (turistic_1 > turistic_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (turistic_1 == turistic_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
-        
+        printf("Attribute used to compare: Density \n"
+               "Density of Card 1: %.2f \n"
+               "Density of Card 2: %.2f \n",
+               density_1, density_2);
+        result1 = density_1 < density_2 ? 1 : 0;
         break;
     case 6:
         printf("Card 1 Country name: %s \n", name_1);
         printf("Card 2 Country name: %s \n", name_2);
-        printf(
-            "Attribute used to compare: Density \n"
-            "Density of Card 1: %.2f \n"
-            "Density of Card 2: %.2f \n",
-            density_1, density_2);
-        if (density_1 < density_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (density_1 == density_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
-        
-        break;
-    case 7:
-        printf("Card 1 Country name: %s \n", name_1);
-        printf("Card 2 Country name: %s \n", name_2);
-        printf(
-            "Attribute used to compare: Per Capita \n"
-            "Per Capita of Card 1: %.2f \n"
-            "Per Capita of Card 2: %.2f \n",
-            per_capita_1, per_capita_2);
-        if (per_capita_1 > per_capita_2)
-        {
-            printf("Card 1 Won! \n");
-        }
-        else if (per_capita_1 == per_capita_2)
-        {
-            printf("It's a draw! \n");
-        }
-        else
-        {
-            printf("Card 2 Won! \n");
-        }
-        
+        printf("Attribute used to compare: Per Capita \n"
+               "Per Capita of Card 1: %.2f \n"
+               "Per Capita of Card 2: %.2f \n",
+               per_capita_1, per_capita_2);
+        result1 = per_capita_1 > per_capita_2 ? 1 : 0;
         break;
     default:
         printf("Invalid option! \n Try again! \n");
     }
 
+    printf("Enter a number of the attribute with which you will compare last: \n"
+           "1 = Population || 2 = Area || 3 = PIB || 4 = Turistic || 5 = Density || 6 = Per Capita \n");
+
+    int option2;
+    int result2;
+    scanf("%d", &option2);
+
+    if (option1 == option2)
+    {
+        printf("You can't compare the same attribute twice! \n");
+        return 0;
+    }
+    else
+    {
+
+        switch (option2)
+        {
+        case 1:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: Population \n"
+                   "Population of Card 1: %lu \n"
+                   "Population of Card 2: %lu \n",
+                   population_1, population_2);
+            result2 = population_1 > population_2 ? 1 : 0;
+            break;
+        case 2:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: Area \n"
+                   "Area of Card 1: %.2f \n"
+                   "Area of Card 2: %.2f \n",
+                   area_1, area_2);
+            result2 = area_1 > area_2 ? 1 : 0;
+            break;
+        case 3:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: PIB \n"
+                   "PIB of Card 1: %.2f \n"
+                   "PIB of Card 2: %.2f \n",
+                   pib_1, pib_2);
+            result2 = pib_1 > pib_2 ? 1 : 0;
+            break;
+        case 4:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: Turistic \n"
+                   "Turistic of Card 1: %d \n"
+                   "Turistic of Card 2: %d \n",
+                   turistic_1, turistic_2);
+            result2 = turistic_1 > turistic_2 ? 1 : 0;
+            break;
+        case 5:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: Density \n"
+                   "Density of Card 1: %.2f \n"
+                   "Density of Card 2: %.2f \n",
+                   density_1, density_2);
+            result2 = density_1 < density_2 ? 1 : 0;
+            break;
+        case 6:
+            printf("Card 1 Country name: %s \n", name_1);
+            printf("Card 2 Country name: %s \n", name_2);
+            printf("Attribute used to compare: Per Capita \n"
+                   "Per Capita of Card 1: %.2f \n"
+                   "Per Capita of Card 2: %.2f \n",
+                   per_capita_1, per_capita_2);
+            result2 = per_capita_1 > per_capita_2 ? 1 : 0;
+            break;
+        default:
+            printf("Invalid option! \n Try again! \n");
+        }
+    }
+
+    if (result1 && result2)
+    {
+        printf("Card 1 Won! \n");
+    }
+    else if (result1 == result2)
+    {
+        printf("Card 2 Won! \n");
+    }
+    else
+    {
+        printf("It's a draw! \n");
+    }
     return 0;
 }
