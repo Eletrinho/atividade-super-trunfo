6bae99210510f78ad9f44eca8442465eacbec0df
Cauã <cauacustodiopereira@gmail.com>
2025-02-24 19:47:07 -0300

    Tema 3 Desafio Aventureiro

--- tema4/tema4.c
+++ tema4/tema4.c
@@ -13,6 +13,16 @@ int main(int argc, char const *argv[])
                         {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 3, 0, 0, 0, 0, 0, 0, 0, 0}};
 
+    // Navio diagonal 1
+    mapa[1][0] = 3;
+    mapa[2][1] = 3;
+    mapa[3][2] = 3;
+
+    // Navio diagonal 2
+    mapa[7][8] = 3;
+    mapa[8][7] = 3;
+    mapa[9][6] = 3;
+
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
