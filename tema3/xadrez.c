dac1f8b226880ae24e1b31076307aff08f2cd74f
Cauã <cauacustodiopereira@gmail.com>
2025-02-24 17:32:04 -0300

    Tema 3 Desafio Mestre

--- tema3/xadrez.c
+++ tema3/xadrez.c
@@ -1,48 +1,69 @@
 #include <stdio.h>
 
+// Função recursiva para movimentos da torre
+void torre(int n)
+{
+    if (n > 0)
+    {
+        printf("Direita \n");
+        torre(n - 1);
+    }
+}
+// Função recursiva para movimentos do bispo
+void bispo(int n)
+{
+    if (n > 0)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            printf("Cima, ");
+            int j = 0;
+            while (i == 0 && j < 1)
+            {
+                printf("Direita \n");
+                j++;
+            }
+        }
+        bispo(n - 1);
+    }
+}
+// Função recursiva para movimentos da rainha
+void rainha(int n)
+{
+    if (n > 0)
+    {
+        printf("Esquerda \n");
+        rainha(n - 1);
+    }
+}
 int main(int argc, char const *argv[])
 {
     // Movimentos da Torre
     printf("Movimento da Torre: \n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Direita \n");
-    }
+    torre(5);
 
     // Movimento do Bispo
     printf("\n");
     printf("Movimento do Bispo: \n");
-    int bispo = 0;
-    while (bispo < 5)
-    {
-        printf("Cima, Direita \n", bispo+1);
-        bispo++;
-    }
-    
+    bispo(5);
 
     // Movimento da Rainha
     printf("\n");
     printf("Movimento da Rainha: \n");
-    int rainha = 0;
-    do
-    {
-        printf("Esquerda %d \n", rainha+1);
-        rainha++;
-    } while (rainha < 8);
-    
-    // Movimento do Cavalo
+    rainha(8);
+
+    // Movimento do Cavalo com loop complexo
     printf("\n");
     printf("Movimento do Cavalo: \n");
-    for (int i = 0; i < 2; i++)
+    for (int i = 0, j = 0; i < 2; i++)
     {
-        printf("Baixo \n");
-        int j = 0;
-        while (i == 1 && j < 1)
+        printf("Cima \n");
+        if (j == 1)
         {
-            printf("Esquerda \n");
-            j++;
+            printf("Direita \n");
         }
+        j++;
     }
-    
+
     return 0;
 }
